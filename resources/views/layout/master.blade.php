f445486dde5a8d3cf7c3bee702f3ff366ad8524a
khaled <bmask2020@gmail.com>
2023-06-17 17:28:01 +0200

    Sessions, Errors & Flash Messages

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task List Project</title>
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    @yield('styles')
 </head>
 <body>
     <h1 style="margin:1rem">@yield('title')</h1>
     <div style="margin:0 1rem">
+
+        @if(session()->has('success'))
+
+        <div>{{session('success')}}</div>
+
+        @endif
+        
         @yield('content')
     </div>
+    
    
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::post('/tasks/store', function(Request $request){
     
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id]);
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task Created Successfully!');
 
 })->name('tasks.store');
 
