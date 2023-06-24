fba6f0fb94d764ceea31f046dc3478008e7f6a21
khaled <bmask2020@gmail.com>
2023-06-24 12:33:15 +0200

    Styling Forms

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,6 +11,22 @@
         .btn {
             @apply rounded-md px-2 py-1 text-center font-medium shadow-sm text-slate-700 ring-1 ring-slate-700/10 hover:bg-slate-50
         }
+
+        label {
+
+            @apply block uppercase text-slate-700 mb-2
+        }
+
+
+        input , textarea {
+
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+        }
+
+        .error {
+
+            @apply text-red-500 text-sm
+        }
     </style>
      {{-- blade-formatter-enable --}}
     @yield('styles')
--- resources/views/tasks/form.blade.php
+++ resources/views/tasks/form.blade.php
@@ -2,21 +2,6 @@
 
 @section('title', isset($tasks) ? 'Edit Task' : 'Add Task')
 
-@section('styles')
-
-<style>
-
-  .error-message {
-
-    color:red;
-    font-size:0.8rem
-
-  }
-
-</style>
-
-@endsection
-
 @section('content')
 <form action="{{isset($task) ? route('tasks.update', ['task' => $tasks->id]) : route('tasks.store')}}" method="post">
 @csrf
@@ -26,28 +11,28 @@
 @endisset
 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Title</label>
-    <input type="text" name="title" class="form-control" value="{{$tasks->title ?? old('title')}}" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" @class(['border-red-500' => $errors->has('title')]) name="title" class="form-control" value="{{$tasks->title ?? old('title')}}" id="exampleInputEmail1" aria-describedby="emailHelp">
    @error('title')
-    <p class="error-message">{{ $message }}</p>
+    <p class="error">{{ $message }}</p>
    @enderror
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Description</label>
-    <textarea name="description" class="form-control">{{$tasks->description ?? old('description')}}</textarea>
+    <textarea name="description" @class(['border-red-500' => $errors->has('description')])>{{$tasks->description ?? old('description')}}</textarea>
     @error('description')
-    <p class="error-message">{{ $message }}</p>
+    <p class="error">{{ $message }}</p>
     @enderror
   </div>
 
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Long Description</label>
-    <textarea name="long_description" class="form-control">{{$tasks->long_description ?? old('long_description')}}</textarea>
+    <textarea name="long_description" @class(['border-red-500' => $errors->has('long_description')])>{{$tasks->long_description ?? old('long_description')}}</textarea>
     @error('long_description')
-    <p class="error-message">{{ $message }}</p>
+    <p class="error">{{ $message }}</p>
     @enderror
   </div>
   
-  <button type="submit" class="btn btn-primary">
+  <button type="submit" class="btn">
     @isset($tasks)
     Update Task 
     @else 
